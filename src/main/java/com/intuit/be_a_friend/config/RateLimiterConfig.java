3a433a95c79848e61b46a04789bf949fa921391b
namrutha.h <namrutha.h@flipkart.com>
2024-09-20 11:32:23 +0530

    Updating RateLimiter at userLevel

--- src/main/java/com/intuit/be_a_friend/config/RateLimiterConfig.java
+++ src/main/java/com/intuit/be_a_friend/config/RateLimiterConfig.java
@@ -4,7 +4,6 @@ import io.github.bucket4j.Bandwidth;
 import io.github.bucket4j.Bucket;
 import io.github.bucket4j.Bucket4j;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 import java.util.HashMap;
@@ -13,14 +12,20 @@ import java.util.Map;
 @Configuration
 public class RateLimiterConfig {
 
-    private final Map<String, Bucket> buckets = new HashMap<>();
+    private final Map<String, Bucket> ipBuckets = new HashMap<>();
+    private final Map<String, Bucket> userBuckets = new HashMap<>();
 
-
-    public Bucket resolveBucket(String key) {
-        // Create a new bucket with a rate limit of 10 requests per minute if one does not already exist
-        return buckets.computeIfAbsent(key, k -> Bucket4j.builder()
+    public Bucket resolveIpBucket(String ip) {
+        // Create a new bucket with a rate limit of 100 requests per minute if one does not already exist
+        return ipBuckets.computeIfAbsent(ip, k -> Bucket4j.builder()
                 .addLimit(Bandwidth.simple(100, Duration.ofMinutes(1)))
                 .build());
     }
 
+    public Bucket resolveUserBucket(String username) {
+        // Create a new bucket with a rate limit of 10 requests per minute if one does not already exist
+        return userBuckets.computeIfAbsent(username, k -> Bucket4j.builder()
+                .addLimit(Bandwidth.simple(10, Duration.ofMinutes(1)))
+                .build());
+    }
 }
